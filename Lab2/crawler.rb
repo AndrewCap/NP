a7bd02ebe3762af67d683da8c004479a5a747f43
andreicap <andrei.capastru@gmail.com>
2015-06-16 18:37:13 +0300

    Implemented Crawling by filetype

--- Lab2/crawler.rb
+++ Lab2/crawler.rb
@@ -1,14 +1,36 @@
 require 'socket'
 
-host = 'info.railean.net'
+host = 'torrentsmd.com'
 port = 80
 
 s = TCPSocket.open host, port
-s.puts "GET / HTTP/1.1\r\n"
+s.puts "GET / HTTP/1.0\r\n"
 s.puts "\r\n"
 
-while line = s.gets
+content_type = Hash.new
+content_type = {
+  'text/html' => 'html',
+  'text/plain' => 'txt',
+  'image/png' => 'png',
+  'image/jpeg' => 'jpg'
+}
+
+filetype = ''
+
+8.times  do	
+	line = s.gets
   puts line.chop
+  if line.include?("Content")
+  	filetype = content_type[line.split[1][0..-2]]
+  end
 end
+
+page = File.open("index.#{filetype}", 'r+')
+
+while line = s.gets 
+	page.puts(line.chop)
+end
+
+page.close
 
 s.close
